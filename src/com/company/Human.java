eda58734704608d3f03a59b34018ecafaa22c82d
spetseffect <spetseffect@gmail.com>
2021-03-04 12:14:21 +0200

    start

---
+++ src/com/company/Human.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Human {
+    public String fname;
+    public String lname;
+    public int age;
+    public Human(){}
+    public Human(String fn, String ln, int ag){
+        this.fname=fn;
+        this.lname=ln;
+        this.age=ag;
+    }
+}
