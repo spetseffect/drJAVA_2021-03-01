79c14fca70989abe222f8e255593dbb38949f28b
spetseffect <spetseffect@gmail.com>
2021-03-06 01:25:19 +0200

    add class Product (fully operational)

---
+++ src/com/Money/Product.java
@@ -0,0 +1,30 @@
+package com.Money;
+
+public class Product {
+    private String name;
+    private Money price;
+    //there can be any number parameters here
+    public Product() {}
+    public Product(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Money getPrice() {
+        return price;
+    }
+    public void setPrice(Money price) {
+        this.price = price;
+    }
+    public void priceUp(double value){
+        this.price.addition(value);
+    }
+    public void priceDown(double value){
+        this.price.subtraction(value);
+    }
+}
